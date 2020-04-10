d189f1c9d45fe95cc50d9c8e8f87711694967ff5
Nataliya Pogodina <n.pogodina@gmail.com>
2020-04-10 09:05:44 -0700

    Added components: Todos, TodoItem and the functionality to mark TodoItems completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Red Adidas Track Suit for Cody',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Lego 3+ for Evelynna',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Sorel boots for Nataliya',
+        completed: false
+      }
+    ]
+  }
+
+  // Toggle Complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete}/>
+      </div>
+    );
+  }
 }
 
 export default App;
