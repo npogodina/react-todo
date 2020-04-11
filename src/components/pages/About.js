c33f4624a4fd1b1aecec72c29034fd4b718ab80f
Nataliya Pogodina <n.pogodina@gmail.com>
2020-04-11 07:01:39 -0700

    Added routes: / and /about

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is Christmas TODO app v2.0.0. Based on React Crash Course by Traversy Media.</p>
+    </React.Fragment>
+  )
+}
